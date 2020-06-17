0a216c95262bb2e338903c1654849011194eb5ce
gcampbell391 <gcampbell391@gmail.com>
2020-06-17 15:57:41 -0400

    Ready for Deploy

--- public/js/app.js
+++ public/js/app.js
@@ -7,16 +7,23 @@ const weatherIcon = document.querySelector('#forecast_icon')
 
 
 weatherForm.addEventListener('submit', (event) => {
+    messageOne.innerHTML = 'Loading...'
     event.preventDefault()
     const address = event.target.querySelector('input').value
 
     fetch(`http://localhost:3000/weather?address=${address}`)
         .then(resp => resp.json())
         .then(data => {
-            console.log(data)
-            messageOne.innerHTML = data.location
-            messageTwo.innerHTML = data.forecastData.description
-            weatherIcon.src = data.forecastData.forecast_icon
+            if (data.error) {
+                messageOne.innerHTML = data.error
+                messageTwo.innerHTML = ""
+                weatherIcon.src = ""
+            }
+            else {
+                messageOne.innerHTML = data.location
+                messageTwo.innerHTML = data.forecastData.description
+                weatherIcon.src = data.forecastData.forecast_icon
+            }
         })
     weatherForm.reset();
-})
+}) 
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,10 +4,13 @@ const request = require('postman-request');
 const geocode = (address, callback) => {
     const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${GEOCODE_API_KEY}&limit=1`
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback("Unable to connect to location services...Please check your connection and try again.", undefined)
         } else if (body.message === 'Not Found') {
+            callback("Unable to find location. Please try again.", undefined)
+        }
+        else if (body.features.length === 0) {
             callback("Unable to find location. Please try again.", undefined)
         }
         else {
